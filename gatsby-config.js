24729eec5f32343696563fc6a0ce7b671364e458
lizzSoup <elizabeth.ann.soup@gmail.com>
2020-09-06 16:11:04 -0400

    Gatsby and mdx images

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,12 +7,15 @@ module.exports = {
   plugins: [
     'gatsby-plugin-emotion',
     'gatsby-plugin-react-helmet',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     {
       resolve: 'gatsby-plugin-mdx',
       options: {
         defaultLayouts: {
           default: require.resolve('./src/components/layouts.js'),
         },
+        gatsbyRemarkPlugins: [{ resolve: 'gatsby-remark-images' }],
       },
     },
     {
@@ -20,6 +23,13 @@ module.exports = {
       options: {
         name: 'posts',
         path: 'posts',
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: 'images',
       },
     },
   ],
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Layout from '../components/layouts';
+import Hero from '../components/hero';
 import usePosts from '../hooks/use-posts';
 import PostPreview from '../components/post-preview';
 import ReadLink from '../components/read-link';
@@ -8,15 +9,14 @@ export default () => {
   const posts = usePosts();
 
   return (
-    <Layout>
-      <h1>Home</h1>
-      <p>Hello Toronto!</p>
-      <ReadLink to="/about/">Learn About Me &rarr;</ReadLink>
-
-      <h2>Read my Blog</h2>
-      {posts.map(post => (
-        <PostPreview key={post.slug} post={post} />
-      ))}
-    </Layout>
+    <>
+      <Hero />
+      <Layout>
+        <h2>Read my Blog</h2>
+        {posts.map(post => (
+          <PostPreview key={post.slug} post={post} />
+        ))}
+      </Layout>
+    </>
   );
 };
