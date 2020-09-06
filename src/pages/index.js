61189beafa87ef3f3c2cd43a745ac7b087ba22d7
lizzSoup <elizabeth.ann.soup@gmail.com>
2020-09-06 00:49:07 -0400

    Mdx blog

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import { Link } from 'gatsby';
 import Layout from '../components/layouts';
+import usePosts from '../hooks/use-posts';
+import PostPreview from '../components/post-preview';
+import ReadLink from '../components/read-link';
 
-export default () => (
-  <Layout>
-    <h1>Home</h1>
-    <p>Hello Toronto!</p>
-    <Link to="/about/">Learn About Me &rarr;</Link>
-  </Layout>
-);
+export default () => {
+  const posts = usePosts();
+
+  return (
+    <Layout>
+      <h1>Home</h1>
+      <p>Hello Toronto!</p>
+      <ReadLink to="/about/">Learn About Me &rarr;</ReadLink>
+
+      <h2>Read my Blog</h2>
+      {posts.map(post => (
+        <PostPreview key={post.slug} post={post} />
+      ))}
+    </Layout>
+  );
+};
