d2d73e171e490794752d6f013d49661573396bc2
lizzSoup <elizabeth.ann.soup@gmail.com>
2020-09-07 18:53:09 -0400

    About page copy

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,7 +5,14 @@ import Layout from '../components/layouts';
 export default () => (
   <Layout>
     <h1>About Me</h1>
-    <p>This is my personal website</p>
+    <p>
+      Frontend web developer with 2 years of experience studying Frontend Web
+      Development, User Interface Fundamentals, and User Experience Theory, and
+      over 2 years experience leading on a management team. I focus on building
+      products with a strong focus on industry standards, usability, and
+      accessibility.
+    </p>
+
     <Link to="/">&larr; Back to Home</Link>
   </Layout>
 );
